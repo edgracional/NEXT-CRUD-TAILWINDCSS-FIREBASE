72d3d18026e99a67d364be67432354114f0d203d
Eder Gracional <Emailedergacional@hotmail.com>
2022-03-23 14:42:46 -0300

    105. Configuração: NextJS, TailwindCSS e Firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
     <div className={`
       flex h-screen justify-center items-center
-      bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600
+      bg-gradient-to-r from-purple-500 via-yellow-400 to-blue-600
     `}>
       <span className="text-4xl">Texto</span>
     </div>
