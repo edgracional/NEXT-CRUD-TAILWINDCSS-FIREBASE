3eeb028ab63db7acf742cf419058607898787ce0
Eder Gracional <Emailedergacional@hotmail.com>
2022-04-04 09:36:20 -0300

    118. Organizando Código com Hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,24 @@
-import { useEffect, useState } from 'react';
 import Botao from '../components/Botao';
 import Formulario from '../components/Formulario';
 import Layout from './../components/Layout';
 import Tabela from './../components/Tabela';
-import Cliente from './../core/Cliente';
 
-import ClienteRepositorio from '../core/ClienteRepositorio';
-import ColecaoCliente from './../backend/db/ColecaoCliente';
+
+import useCleintes from './../hooks/useClientes';
 
 
 export default function Home() {
 
-  const repo: ClienteRepositorio = new ColecaoCliente()
-
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const [clientes, setClientes] = useState<Cliente[]>([])
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-
-  useEffect(obterTodos, [])
-
-  function obterTodos(){
-    repo.obterTodos().then(clientes => {
-      setClientes(clientes)
-      setVisivel('tabela')
-    })
-  }
-
-function clienteSelecionado(cliente: Cliente) {
-  setCliente(cliente)
-  setVisivel('form')
-}
- async function clienteExcluido(cliente: Cliente) {
-  await repo.excluir(cliente)
-  obterTodos()
-}
-
-function novoCliente(){
-  setCliente(Cliente.vazio())
-  setVisivel('form')
-}
-
-async function salvarCliente(cliente: Cliente){
-  await repo.salvar(cliente)
-  obterTodos()
-}
-
-/* Criar um estado - definir um useStade com dois possíveis estados
-e como padrão iniciara como tabela */
+  const { 
+    cliente,
+    clientes,
+    novoCliente,
+    salvarCliente,
+    selecionarCliente,
+    excluirCliente,
+    exibirTabela,
+    tabelaVisivel
+  } = useCleintes()
 
 
   return (
@@ -59,7 +31,7 @@ e como padrão iniciara como tabela */
 
       <Layout titulo="Cadastro Simples">
         {/* colcoar um renderização opcional */}
-        {visivel === 'tabela' ? (
+        {tabelaVisivel ? (
           
         /* colocar o botão e tabela dentro de um fragmento <> </> */
         <>
@@ -71,8 +43,8 @@ e como padrão iniciara como tabela */
         </div>
           
           <Tabela clientes={clientes}
-              clienteSelecionado={clienteSelecionado}
-              clienteExcluido={clienteExcluido}
+              clienteSelecionado={selecionarCliente}
+              clienteExcluido={excluirCliente}
         />
         </>
 
@@ -80,7 +52,7 @@ e como padrão iniciara como tabela */
           <Formulario 
             cliente={cliente}
             clienteMudou={salvarCliente}
-            cancelado={() => setVisivel('tabela')}
+            cancelado={() => exibirTabela}
           />
         )}
 
